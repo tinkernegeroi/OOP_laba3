12d8d3a30fcbd695481fe5050584bc2e0a5e4d0a
Roman <roman.peskoff@gmail.com>
2026-03-03 03:59:47 +0300

    Initial commit

---
+++ OOP_laba2/Form2.cs
@@ -0,0 +1,23 @@
+﻿namespace OOP_laba2;
+
+public partial class Form2 : Form
+{
+    public Form2()
+    {
+        InitializeComponent();
+    }
+    private void button_Back_Click(object sender, EventArgs e)
+    {
+        this.Close();
+    }
+
+    private void button_Exit_Click(object sender, EventArgs e)
+    {
+        Application.Exit();
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ OOP_laba2/src/exceptions/AirportInvalidCastException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_laba2.exceptions;
+
+public class AirportInvalidCastException : Exception
+{
+    public AirportInvalidCastException() 
+        : base("Ошибка приведения типа.") { }
+
+    public AirportInvalidCastException(string message) 
+        : base(message) { }
+}
